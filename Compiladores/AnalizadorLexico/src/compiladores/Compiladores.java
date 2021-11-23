28f68be14d84962c688a3997d15bc0eeced168f7
FranzFlores <andresfloresgallardo@hotmail.com>
2021-11-23 17:25:09 -0500

    Correciones

--- Compiladores/AnalizadorLexico/src/compiladores/Compiladores.java
+++ Compiladores/AnalizadorLexico/src/compiladores/Compiladores.java
@@ -11,12 +11,9 @@ package compiladores;
  */
 public class Compiladores {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Funcioan");
+        System.out.println("Funciona");
     }
-    
+
 }
